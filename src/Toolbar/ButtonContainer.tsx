2ac88fffffd2c9eb1bfc8df4e51322b0f87310ee
leehyunggeun <leehyunggeun@navercorp.com>
2018-06-02 17:38:29 +1000

    add an interface, and set onClick event and effect property

--- src/Toolbar/ButtonContainer.tsx
+++ src/Toolbar/ButtonContainer.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
-// import Button from '../com'
+import Button from 'common/Button';
 
-class ButtonContainer extends React.Component {
-  render() {
+interface IButtonContainerProps {
+  onClick: (args: any) => void;
+  effect: string;
+}
+
+class ButtonContainer extends React.Component<IButtonContainerProps> {
+  constructor(props: IButtonContainerProps) {
+    super(props);
+  }
+  public render() {
     return (
-
+      <Button onClick={this.props.onClick.bind(this, this.props.effect)}>
+        {this.props.effect}
+      </Button>
     )
   }
 }
