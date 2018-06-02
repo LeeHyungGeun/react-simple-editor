a86066ec9930f53262b76856331a1eddb8d2ee39
leehyunggeun <leehyunggeun@navercorp.com>
2018-06-02 17:37:48 +1000

    add four buttons: Bold, Italic, Underline, Strike

--- src/Toolbar/ToolbarContainer.tsx
+++ src/Toolbar/ToolbarContainer.tsx
@@ -1,32 +1,27 @@
 import * as React from 'react';
-import { Button } from '../common';
+import Button from './ButtonContainer';
 import './toolbar.css';
 
 interface IProps {
 };
 interface IState {
 };
-interface IButtonClick {
-  effect: string;
-}
-
 class ToolbarContainer extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.onClick = this.onClick.bind(this);
   }
-  public onClick(args: IButtonClick) {
-    const { effect } = args;
-    alert(effect);
+  public onClick(effect: string): void {
+    console.log(effect);
   }
   public render() {
     return (
       <div className="toolbar-container">
         <div className="toolbar-row">
-          <Button onClick={this.onClick}>Bold</Button>
-          <Button onClick={this.onClick}>Italic</Button>
-          <Button onClick={this.onClick}>Underline</Button>
-          <Button onClick={this.onClick}>Strike</Button>
+          <Button onClick={this.onClick} effect="bold">Bold</Button>
+          <Button onClick={this.onClick} effect="italic">Italic</Button>
+          <Button onClick={this.onClick} effect="underline">Underline</Button>
+          <Button onClick={this.onClick} effect="strike">Strike</Button>
         </div>
       </div>
     )
