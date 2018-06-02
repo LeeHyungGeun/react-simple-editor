8afc12b35f3c9dbcf317faa2aaccd529156675cc
leehyunggeun <leehyunggeun@navercorp.com>
2018-06-02 18:09:20 +1000

    edit style of Button

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -7,12 +7,17 @@ interface IButton {
 
 const Button = styled<IButton, any>('div')`
   font-size: 16px;
+  min-width: 1rem;
+  text-align: center;
   padding: 0.5em;
   background: ${theme.color.white};
   border: 1px solid ${theme.color.grey};
   outline: none;
   user-select: none;
   cursor: pointer;
+  &:hover {
+    background: ${theme.color.whiteGrey};
+  }
 `;
 
 export default Button;
