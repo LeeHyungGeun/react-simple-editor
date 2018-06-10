cdb973e95cd3913649ca564c5f0652354db3bfed
leehyunggeun <leehyunggeun@navercorp.com>
2018-06-10 23:26:32 +1000

    add style of Editor frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
+import './index.css';
 import './App.css';
 import Toolbar from './Toolbar';
-import './index.css';
+import Editor from './Editor';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="app">
-        <div className="editor">
-          <Toolbar />
+        <div className="simple-editor-container">
+          <div className="simple-editor-toolbar">
+            <Toolbar />
+          </div>
+          <div className="simple-editor-content">
+            <Editor />
+          </div>
         </div>
       </div>
     );
